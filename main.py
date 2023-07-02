d42c9dfb332e48776603d2fc5bf1f3718d6a49a3
munyuawilson <wmunyua4@gmail.com>
2023-07-02 18:05:58 +0300

    start

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-ap
+app = QApplication([])
+# Create a QMainWindow
+window = QMainWindow()
+# Create a QLabel widget
+label = QLabel("Hello, PyQt5!", parent=window)
+label.setGeometry(50, 50, 200, 50)  # Set the position and size of the label
+
+# Show the main window
+window.show()
+
+# Run the application's event loop
+app.exec()
