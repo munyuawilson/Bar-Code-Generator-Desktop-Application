b442a3732f35cbf0667a6a94512b37c3adc24718
munyuawilson <wmunyua4@gmail.com>
2023-07-03 12:35:01 +0300

    main window update

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtCore import Qt
 
-app = QApplication([])
+app = QApplication(sys.argv)
+
 # Create a QMainWindow
 window = QMainWindow()
-# Create a QLabel widget
-label = QLabel("Hello, PyQt5!", parent=window)
-label.setGeometry(50, 50, 200, 50)  # Set the position and size of the label
+window.setWindowTitle("Bar Code Generator")
+
+# Create a QVBoxLayout to hold the QLabel and text label
+layout = QVBoxLayout()
+
+# Add the image QLabel
+image_label = QLabel()
+pixmap = QPixmap("logo.png")  # Replace with your image path
+image_label.setPixmap(pixmap)
+image_label.setAlignment(Qt.AlignCenter)
+layout.addWidget(image_label)
+
+# Add the text QLabel
+text_label = QLabel("Unlock Your Products' Potential with Seamless Bar Code Generation!")
+text_label.setAlignment(Qt.AlignCenter)
+text_label.setFont(QFont("Arial", 14))  # Increase font size (adjust as needed)
+text_label.setMargin(1)  # Adjust the margin around the text label
+layout.addWidget(text_label)
+
+# Create a QWidget to hold the layout
+central_widget = QWidget()
+central_widget.setLayout(layout)
+window.setCentralWidget(central_widget)
 
 # Show the main window
 window.show()
 
-# Run the application's event loop
-app.exec()
+# Start the application event loop
+sys.exit(app.exec_())
