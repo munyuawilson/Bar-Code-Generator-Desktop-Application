904e6bc26c66cd8f84c9515334ef81253cc74cf8
munyuawilson <wmunyua4@gmail.com>
2023-07-05 13:23:58 +0300

    update genbar

--- genbar.py
+++ genbar.py
@@ -1,9 +1,13 @@
 import barcode
 from barcode.writer import ImageWriter
+
 def generate_barcode(text, barcode_type='code128'):
     # Create a barcode object
     barcode_class = barcode.get_barcode_class(barcode_type)
-    barcode_object = barcode_class(text, writer=ImageWriter())
+
+    # Disable the text option in the writer
+    writer_options = {'text': None}
+    barcode_object = barcode_class(text, writer=ImageWriter(), writer_options=writer_options)
 
     # Set the filename for the barcode image
     filename = f'barcode_{text}'
